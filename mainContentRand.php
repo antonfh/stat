fa288f8c249fb2b3bfee29e8c38fca9045f3a1ce
Anton Heuschen <anton@pdmssa.com>
2016-11-01 10:06:15 +0200

    t2

--- mainContentRand.php
+++ mainContentRand.php
@@ -1,6 +1,6 @@
 
 <?php
-
+header("Access-Control-Allow-Origin: http://pdmsssa.dev");
 
 $ddata[0] = '
 {"series":[
--- mainContentRandOneLine.php
+++ mainContentRandOneLine.php
@@ -1,5 +1,5 @@
 <?php
-
+header("Access-Control-Allow-Origin: http://pdmsssa.dev");
 $ddata[0] = '
 {
     "series":[89],
