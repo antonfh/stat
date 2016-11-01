5257f10ac24d69e042ddcb0b1e9f6027d433109c
Anton Heuschen <anton@pdmssa.com>
2016-11-01 14:33:00 +0200

    t2

--- mainContentRand.php
+++ mainContentRand.php
@@ -2,32 +2,15 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-$ddata[0] = '
-{"series":[
-[25],
-[68]
-  ],
-  "labels" : ["09:00"]
-}
-';
 
-$ddata[1] = '
+$ddata = '
 {"series":[
-[75],
-[-28]
-  ],
-  "labels" : ["10:00"]
-}
-';
-
-$ddata[2] = '
-{"series":[
-[15],
-[38]
+[ ' . rand(-100, 100) . '],
+[ ' . rand(-100, 100) . ']
   ]
-  ,"labels" : ["11:00"]
+  ,"labels" : [ ' . Date("H:i") . ']
 }
 ';
 
 
-echo $ddata[rand(0,2)];
+echo $ddata;
