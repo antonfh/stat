07407e26f956ef904153dda258d39ff04c8df9f6
Anton Heuschen <anton@pdmssa.com>
2016-11-03 16:30:00 +0200

    chart updates

--- mainContentRand.php
+++ mainContentRand.php
@@ -2,14 +2,14 @@
 header("Access-Control-Allow-Origin: *");
 date_default_timezone_set('Africa/Johannesburg');
 
-$ddata = "
-{'series':[
-['" . rand(-100, 100) . "'],
-['" . rand(-100, 100) . "']
+$ddata =<<< EOT
+{"series":[
+['rand(-100, 100)'],
+['rand(-100, 100)']
   ]
-  ,'labels' : ['" . Date("H:i") . "']
+  ,"labels" : ['Date("H:i")']
 }
-";
+EOT;
 
 
 echo $ddata;
--- mods/getData.js
+++ mods/getData.js
@@ -6,8 +6,10 @@ function getData(call, $http) {
     return $http({method: 'GET', url: 'http://psapi.anton.co.za/' + call + ''})
         .success(function (data, status, headers, config, scopedataset) {
             sc = {scopedata: data['series'], scopelabels: data['labels']};
+            console.log('getData-' + call);
+            console.log(data);
         })
         .error(function (data, status, headers, config) {
-            console.log('Data error' + status)
+            console.log('Data error' + status + ' Data- ' . data)
         });
 };
