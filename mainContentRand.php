f9cc35ab91979d5254fabdd537f0f366cf7cc77c
Anton Heuschen <anton@pdmssa.com>
2016-11-04 09:10:30 +0200

    g

--- mainContentRand.php
+++ mainContentRand.php
@@ -1,13 +1,16 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 date_default_timezone_set('Africa/Johannesburg');
+$rnd1 = rand(-100, 100);
+$rnd2 = rand(-100, 100);
+$dte = Date("H:i");
 
 $ddata =<<< EOT
 {"series":[
-['rand(-100, 100)'],
-['rand(-100, 100)']
+['$rnd1'],
+['$rnd2']
   ]
-  ,"labels" : ['Date("H:i")']
+  ,"labels" : ['$dte']
 }
 EOT;
 
--- mainContentRandOneLine.php
+++ mainContentRandOneLine.php
@@ -2,13 +2,15 @@
 header("Access-Control-Allow-Origin: *");
 date_default_timezone_set('Africa/Johannesburg');
 
-$ddata = '
-{"series":[
-' . rand(-100, 100) . '
-  ]
-  ,"labels" : ["' . Date("H:i") . '"]
-}
-';
+$rnd1 = rand(-100, 100);
+$rnd2 = rand(-100, 100);
+$dte = Date("H:i");
 
+$ddata =<<< EOT
+{
+    "series":['$rnd1'],
+    "labels":['$dte']
+}
+EOT;
 
 echo $ddata;
